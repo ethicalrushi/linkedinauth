061d6a61a1a5cae146664ccc8e252e8f19ec7b88
Rushikesh <pupalerushikesh@gmail.com>
2018-03-30 15:28:43 +0530

    Initial commit

---
+++ link.js
@@ -0,0 +1,25 @@
+ function onload() {
+    IN.Event.on(IN, "auth", onLinkedInAuth);
+    $('#profiles').css('display','none');
+
+  }
+  function onLinkedInAuth() {
+
+    IN.API.Profile("me").fields("first-name", "last-name","picture-url").result(displayProfiles);
+    $('#profiles').css('display','none');
+  }
+
+  function displayProfiles(profiles) {
+    member = profiles.values[0];
+    
+    
+	$('#profiles').css('display','block');
+	$('#pic').attr('src',member.pictureUrl);
+	$('#fname').text(member.firstName);
+	$('#lname').text(member.lastName);
+  }
+
+  function closesession(){
+  	IN.User.logout();
+   	return true;
+  }
